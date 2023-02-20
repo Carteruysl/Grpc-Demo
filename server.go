31b1fb156cf5031c4c9c2fb19c08e717222c82d5
Carteruysl <wcarter9527@gmail.com>
2023-02-20 10:38:43 +0800

    Add files via upload

---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	poker "grpc-demo/poker"
+	pb "grpc-demo/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedPokerServer
+}
+
+func (s *server) GetNuts(ctx context.Context, req *pb.GetNutsRequest) (*pb.GetNutsResponse, error) {
+	res, err := poker.PokerEvaluator(req.Hand, req.River)
+	return &pb.GetNutsResponse{
+		Card: res,
+	}, err
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPokerServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
